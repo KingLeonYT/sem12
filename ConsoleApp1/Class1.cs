73953000bba43d4855cb45f5ef62c36363027526
KingLeonYT <tannersrn1@gmail.com>
2024-10-31 12:01:37 -0500

    Insertar

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,38 @@
+﻿namespace ejercicio
+{
+    internal class Estudiantes
+    {
+        byte[] edades = new byte[0];
+        int posiciones = 0;
+        Estudiantes en = new Estudiantes();
+
+        public void insertar()
+        {
+            string seguir;
+            byte ed;
+            do
+            {
+                Console.WriteLine("Ingrese una edad");
+                while (!byte.TryParse(Console.ReadLine(), out ed) || ed > 120 || ed < 14)
+                {
+                    Console.WriteLine("Error!!!!");
+                    Console.WriteLine("Ingrése nuevamente el dato");
+                }
+                Array.Resize(ref edades, edades.Length + 1);
+                edades[posiciones] = ed;
+                posiciones++;
+                Console.WriteLine("Edad registrada correctamente");
+                Console.WriteLine("Desea registrar mas edades");
+                seguir = Console.ReadLine().ToLower();
+
+            } while (seguir == "si");
+        }
+        public void mostrar() 
+        { 
+            Console.SetCursorPosition(10, 13);
+            Console.WriteLine("POSICIÓN");
+            Console.SetCursorPosition(30, 12);
+            Console.WriteLine("EDADES");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using ejercicio;
+byte opcion;
+string regresar;
+
+do
+{
+
+    Console.WriteLine("BIENVENDIOS AL SISTEMA DE REGISTRO DE EDADES\n");
+
+    Console.WriteLine("******* MENÚ *******");
+
+    Console.WriteLine("* 1. Insentar   *");
+
+    Console.WriteLine("* 2. Mostrar    *");
+
+    Console.WriteLine("* 3. Eliminar   *");
+
+    Console.WriteLine("* 4. Ordenar    *");
+
+    Console.WriteLine("* 0. Salir     *");
+
+    Console.WriteLine("********************\n");
+
+
+
+    Console.Write("Ingrese opcion: ");
+
+
+
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 4)
+    {
+
+        Console.Write("Error: Ingrese opcion: ");
+
+    }
+
+
+
+    switch (opcion)
+    {
+
+        case 0: Environment.Exit(0); break;
+
+        case 1:
+            en.insertar();
+                break;
+
+        case 2: break;
+
+        case 3: break;
+
+        case 4: break;
+
+    }
+
+    Console.WriteLine("Desea regresar al menú? [si]: ");
+
+    regresar = Console.ReadLine();
+
+    Console.Clear();
+
+} while (regresar == "si");
